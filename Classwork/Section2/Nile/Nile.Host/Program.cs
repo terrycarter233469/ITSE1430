f741f0bbb53d3a62a99a26a5a084b5a610360701
terry.carter233469 <terry.carter233469@my.tccd.edu>
2017-09-18 17:36:15 -0500

    Section2 reorg

---
+++ Classwork/Section2/Nile/Nile.Host/Program.cs
@@ -0,0 +1,153 @@
+﻿/*
+ * Your Name
+ * ITSE 1430
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nile.Host
+{
+    class Program
+    {
+        // A single line comment
+        static void Main( string[] args )
+        {
+            bool quit = false;
+            do
+            {
+                char choice = GetInput();
+                switch (choice)
+                {
+                    case 'a':
+                    case 'A':
+                        AddProduct();
+                        break;
+
+                    case 'b':
+                    case 'B':
+                        ListProducts();
+                        break;
+
+                    case 'c':
+                    case 'C':
+                        quit = true;
+                        break;
+                };
+            } while (!quit);
+        }
+
+        private static void ListProducts()
+        {
+
+           //string msg = String.Format("{0}\t\t\t{1}\t\t[{3}]", productName, productPrice, productDiscontinued ? "[Discontomied]" : "");
+            string msg = $"{productName}\t\t\t${productPrice}\t\t{(productDiscontinued ? "[Discontinued]" : "")}";      //string interpulation mode
+
+            Console.WriteLine(msg);
+            Console.WriteLine(productDescription);
+        }
+
+        private static void AddProduct()
+        {
+            Console.Write("Enter product name: ");
+            productName = Console.ReadLine().Trim();
+
+            //ensure not empty
+
+            Console.Write("Enter price (>0): ");
+            productPrice = ReadDecimal();
+
+            Console.Write("Enter optional description: ");
+            productDescription = Console.ReadLine().Trim();
+
+            Console.Write("Is it discontinued (Y/N: ");
+            productDiscontinued = ReadYesNo();
+
+            //Name $price[Discontinued]
+            //Description
+        }
+
+        static char GetInput()
+        {
+                while (true)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Main Menu");
+                    Console.WriteLine("".PadLeft(10, '-'));
+                    Console.WriteLine("A) Add product");
+                    Console.WriteLine("B) List product");
+                    Console.WriteLine("C) Quit product");
+
+                    string input = Console.ReadLine().Trim();
+
+                    if (input != String.Empty)  //input != "" and input.Length == 0 both work, but .Length needs to check for null
+                    {
+                        //String Comparion
+                        //if (String.Compare(input, "A", true) == 0) //case insensitive comparison, returns an int (0 is equal, 1 is not equal)
+                        //    return 'A';
+
+                        //Char comparison
+                        char letter = Char.ToUpper(input[0]);
+                        if (letter == 'A')
+                            return 'A';
+                        else if (letter == 'B')
+                            return 'B';
+                        else if (letter == 'C')
+                            return 'C';
+                    };
+                    //Error
+                    Console.WriteLine("Please choose a valid menu option");
+                }
+
+            }
+
+        /// <summary>Reads a decimal from Console.</summary>
+        /// <returns>The decimal value.</returns>
+        static decimal ReadDecimal()
+        {
+            do
+            {
+                string input = Console.ReadLine();
+
+
+                //Output Parameter( out datatype parameter )
+                //    function must be able to write
+                //    function can read after write
+                //    function call passes no value
+                //inline variable declaration only works with out
+                if (Decimal.TryParse(input, out decimal result))
+                    return result;
+
+                Console.WriteLine("Enter a valid decimal");
+            } while (true);
+        }
+
+        static bool ReadYesNo()
+        {
+            do
+            {
+                string input = Console.ReadLine();
+                
+                if(!String.IsNullOrEmpty(input))
+                {
+                    switch (Char.ToUpper(input[0]))
+                    {
+                        case 'Y': return true;
+                        case 'N': return false;
+                    }
+                }
+
+                Console.WriteLine("Enter either Y or N");
+            } while (true);
+        }
+
+
+        //Product
+        static string productName;
+            static decimal productPrice;
+            static string productDescription;
+            static bool productDiscontinued;
+        }
+    }
+
---
+++ Labs/_Lab1/Lab1/Program.cs
@@ -0,0 +1,160 @@
+﻿/*Terry Carter
+ * ITSE1430
+ * 8/16/2017
+ * */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MovieProgram.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool quit = false;
+            do
+            {
+                char choice = GetInput();
+                switch (choice)
+                {
+                    case 'L': ListMovies(); break;
+                    case 'A': AddMovies(); break;
+                    case 'R': RemoveMovies(); break;
+                    case 'Q': quit = true; break;
+                };
+            } while (!quit);
+        }
+
+        private static void AddMovies()
+        {
+            Console.Write("Enter the title of the movie: ");
+            movieTitle = Console.ReadLine().Trim();
+
+            //Ensure not empty
+
+            Console.Write("Enter the length of the movie(Minutes): ");
+            movieLength = ReadLength();
+
+            Console.Write("Enter an optional description: ");
+            movieDescription = Console.ReadLine().Trim();
+
+            Console.Write("Do you own this movie? (Y/N): ");
+            movieOwned = ReadYesNo();
+        }
+
+        private static void ListMovies()
+        {
+            if(movieTitle != "")
+            {
+                Console.WriteLine(movieTitle);
+                Console.WriteLine(movieDescription);
+                string msg = $"Run length = {movieLength} mins";
+                Console.WriteLine(msg);
+                if(movieOwned)
+                    Console.WriteLine("Status = Owned");
+                else
+                    Console.WriteLine("Status = WishList");
+            } else
+                Console.WriteLine("No Movies available");
+        }
+
+        public static void RemoveMovies()
+        {
+            if(!String.IsNullOrEmpty(movieTitle))
+            {
+                movieTitle = "";
+                movieLength = 0;
+                movieDescription = "";
+                movieOwned = false;
+                Console.WriteLine("The move has been removed");
+            } 
+            else
+                Console.WriteLine("Error: There is no movies to remove");
+        }
+
+        static char GetInput()
+        {
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Main Menu");
+                Console.WriteLine("".PadLeft(10, '-'));
+                Console.WriteLine("L)ist Movie");
+                Console.WriteLine("A)dd Movie");
+                Console.WriteLine("R)emove Movie");
+                Console.WriteLine("Q)uit");
+
+                var input = Console.ReadLine().Trim();
+                if (input != null && input.Length != 0)
+                {
+                    //Char comparison
+                    char letter = Char.ToUpper(input[0]);
+                    if (letter == 'A')
+                        return 'A';
+                    else if (letter == 'L')
+                        return 'L';
+                    else if (letter == 'Q')
+                        return 'Q';
+                    else if (letter == 'R')
+                        return 'R';
+                };
+
+                //Error
+                Console.WriteLine("Please choose a valid option");
+            };
+        }
+
+        /// <summary>Reads a boolean from Console.</summary>
+        /// <returns>The boolean value.</returns>
+        static bool ReadYesNo()
+        {
+            do
+            {
+                string input = Console.ReadLine();
+
+                if (!String.IsNullOrEmpty(input))
+                {
+                    switch (Char.ToUpper(input[0]))
+                    {
+                        case 'Y': return true;
+                        case 'N': return false;
+                    };
+                };
+
+                Console.WriteLine("Enter either Y or N");
+            } while (true);
+        }
+
+        static int ReadLength()
+        {
+            int length = 0;
+            bool isNumber = false;
+            while(!isNumber)
+            {
+                string input = Console.ReadLine();
+
+                if(!String.IsNullOrEmpty(input))
+                {
+                    isNumber = Int32.TryParse(input, out length);
+                };
+
+                if(!isNumber || length < 0)
+                {
+                    Console.WriteLine("Enter  a valid length of 0 or higher");
+                    isNumber = false;
+                }
+            }
+
+            return length;
+        }
+
+        //Movie variables
+        static string movieTitle = "";
+        static int movieLength = 0;
+        static string movieDescription = "";
+        static bool movieOwned = false;
+    }
+}
